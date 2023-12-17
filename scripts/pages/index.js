6cd25c99533b1c6516623be71a7528b5750f6fc4
youtlane <youtlanehanane@gmail.com>
2023-12-17 21:03:34 +0100

    feat: homepage cards diplay

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,33 +1,14 @@
+    /*
+        permet de recuperer le contenu du fichier et le transformer en format json et retourner les datas
+    */
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        const data = await fetch("data/photographers.json"); //Récupération de contenu du fichier photographers.json via requete http
+        const dataJson = await data.json(); //transformation du resultat en format json
+        return (dataJson); // retourn les donnees du fichier json
     }
 
     async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+        const photographersSection = document.querySelector(".photographer_section");//recuperer la div photographer_section
 
         photographers.forEach((photographer) => {
             const photographerModel = photographerTemplate(photographer);
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,37 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+        article.setAttribute("id", id);
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", tagline);
+        const h2 = document.createElement('h2');
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
+
+        const localisation = document.createElement('p');
+        localisation.textContent = city + ", " + country;
+        localisation.classList.add('localisation');
+
+        const tag = document.createElement('p');
+        tag.textContent = tagline;
+        tag.classList.add('tag');
+
+        const pricePhoto = document.createElement('p');
+        pricePhoto.textContent = price + "/jour";
+        pricePhoto.classList.add('price');
+
+        article.appendChild(localisation);
+        article.appendChild(tag);
+        article.appendChild(pricePhoto);
+
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, id, getUserCardDOM }
 }
